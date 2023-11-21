be4d4151a4bb437e41c30863b1fa9e752e03998e
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2023-11-21 13:15:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 //your JS code here. If required.
 function submitForm() {
-        var firstName = document.getElementById("firstName").value;
-        var lastName = document.getElementById("lastName").value;
-        var phoneNumber = document.getElementById("phoneNumber").value;
-        var email = document.getElementById("email").value;
+        var firstName = document.getElementById("firstName").type("John");
+        var lastName = document.getElementById("lastName").type("Doe");
+        var phoneNumber = document.getElementById("phoneNumber").type("1234567890");
+        var email = document.getElementById("email").type(john.doe@gmail.com);
 
         var result = "First Name: " + firstName + "\n" +
                      "Last Name: " + lastName + "\n" +
                      "Phone Number: " + phoneNumber + "\n" +
-                     "Email ID: " + email;
+                     "Email ID: " + email.com;
 
         alert(result);
     }
