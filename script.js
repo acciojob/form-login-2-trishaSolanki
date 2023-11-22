580d0d6616cac569bd744d7f75408d814ec4c74b
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2023-11-22 17:21:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
+
 function submitForm(){
 	var FirstName = document.getElementById("firstName").value;
 	var LastName = document.getElementById("lastName").value;
 	var PhoneNumber = document.getElementById("phoneNumber").value;
 	var Email = document.getElementById("email").value;
 
-	alert("First Name: " + firstName);
-	alert("Last Name : " + lastName);
-	alert("Phone Number : " + phoneNumber);
-	alert("Email : " + emil);
+	alert("First Name: " + FirstName);
+	alert("Last Name : " + LastName);
+	alert("Phone Number : " + PhoneNumber);
+	alert("Email : " + Email);
 }
